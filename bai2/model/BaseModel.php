fb7de183fb1bdedf437c1d82b31f1a147f7768b4
ngocbq3 <bqngoc@gmail.com>
2020-08-06 09:47:11 +0700

    update

--- bai2/model/BaseModel.php
+++ bai2/model/BaseModel.php
@@ -9,6 +9,7 @@ class BaseModel
     {
         try {
             $this->conn = new PDO("mysql:host=$this->hostname; dbname=$this->dbname; charset=utf8", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo "Lỗi kết nối cơ sở dữ liệu<br>" . $e->getMessage();
         }
